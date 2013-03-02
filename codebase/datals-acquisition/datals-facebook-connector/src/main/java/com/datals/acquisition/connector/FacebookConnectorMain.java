977c05dfea603fe64fc975cea6044a0b6b5203c5
Dorel Matei <dorel.matei@3pillarglobal.com>
2013-03-02 19:34:06 +0200

    Changed main classes

---
+++ codebase/datals-acquisition/datals-facebook-connector/src/main/java/com/datals/acquisition/connector/FacebookConnectorMain.java
@@ -0,0 +1,13 @@
+package com.datals.acquisition.connector;
+
+public class FacebookConnectorMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

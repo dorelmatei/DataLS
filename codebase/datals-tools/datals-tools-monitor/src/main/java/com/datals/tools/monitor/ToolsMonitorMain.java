a9ded6d5f9922bf6e18fdceac6c791a184dbcd28
Dorel Matei <dorel.matei@3pillarglobal.com>
2013-03-02 19:39:01 +0200

    Changed main classes

---
+++ codebase/datals-tools/datals-tools-monitor/src/main/java/com/datals/tools/monitor/ToolsMonitorMain.java
@@ -0,0 +1,13 @@
+package com.datals.tools.monitor;
+
+public class ToolsMonitorMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ codebase/datals-tools/datals-tools-monitor/src/test/java/com/datals/tools/monitor/ToolsMonitorTest.java
@@ -0,0 +1,13 @@
+package com.datals.tools.monitor;
+
+public class ToolsMonitorTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

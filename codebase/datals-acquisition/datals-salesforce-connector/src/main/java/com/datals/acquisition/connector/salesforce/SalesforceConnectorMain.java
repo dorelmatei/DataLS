fb48b59014e692f169280c934eabf5579baf21ba
Dorel Matei <dorel.matei@3pillarglobal.com>
2013-03-02 19:34:57 +0200

    Changed main classes

---
+++ codebase/datals-acquisition/datals-salesforce-connector/src/main/java/com/datals/acquisition/connector/salesforce/SalesforceConnectorMain.java
@@ -0,0 +1,13 @@
+package com.datals.acquisition.connector.salesforce;
+
+public class SalesforceConnectorMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ codebase/datals-acquisition/datals-salesforce-connector/src/test/java/com/datals/acquisition/connector/saleforce/SalesforceConnectorTest.java
@@ -0,0 +1,13 @@
+package com.datals.acquisition.connector.saleforce;
+
+public class SalesforceConnectorTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

7593e8bede15d249eb0ad64c49ffbe40f9d168d7
Iulian Boanca <boanca.iulian@yahoo.com>
2013-04-07 02:27:55 +0300

    Added Foundation Api & Core modules

---
+++ codebase/datals-foundation/datals-foundation-api/src/main/java/com/datals/foundation/api/FoundationApiMain.java
@@ -0,0 +1,13 @@
+package com.datals.foundation.api;
+
+public class FoundationApiMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ codebase/datals-foundation/datals-foundation-core/src/main/java/com/datals/foundation/core/FoundationCoreMain.java
@@ -0,0 +1,13 @@
+package com.datals.foundation.core;
+
+public class FoundationCoreMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
